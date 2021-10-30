7cc2d287e79a9ff099fe612754ee33428c87b765
felipe-zg <felipe_zeba@outlook.com>
2021-10-30 10:53:37 -0500

    feat: add file with constant values

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,34 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Felipe Zêba on 10/30/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    
+    struct Segues {
+        static let registerSegue = "RegisterToChat"
+        static let loginSegue = "LoginToChat"
+    }
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
